419e712250dc910f03967ea34d525af7f8c075a5
Rodrigo Garoz <garoz.r4@gmail.com>
2020-05-15 15:57:37 -0600

    Testing

--- analysis/models.py
+++ analysis/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class Analysis(models.Model):
     title = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
